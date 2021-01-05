d39864301c4cbdb38c6508f900ed561fe96f8467
Jiří Szkandera <jirik.sz@gmail.com>
2021-01-05 02:41:45 +0100

    Enable color emoji

--- config.h
+++ config.h
@@ -9,7 +9,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = {
+	"monospace:size=12" ,
+	"Noto Color Emoji:pixelsize=12"
+};
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#0f1117";
 static const char col_gray2[]       = "#0f1117";
